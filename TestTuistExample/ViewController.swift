f8ed8268b374a20d716071d6e6e7aa91ff36f181
Vadim <vadim.seregin@raiffeisen.ru>
2023-02-08 09:03:50 +0300

    Initial Commit

---
+++ TestTuistExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTuistExample
+//
+//  Created by SEREGIN Vadim on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
